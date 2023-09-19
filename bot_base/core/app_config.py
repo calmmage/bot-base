ae8582657c18dbbb3e372a590ebe6bfbae75502f
Petr Lavrov <calmquant@gmail.com>
2023-09-19 03:55:39 +0300

    Update for new pydantic config syntax

--- bot_base/core/app_config.py
+++ bot_base/core/app_config.py
@@ -8,9 +8,10 @@ class DatabaseConfig(BaseSettings):
     conn_str: str
     name: str
 
-    class Config:
-        extra = 'ignore'
-        env_prefix = 'DATABASE_'
+    model_config = {
+        "extra": "ignore",
+        "env_prefix": "DATABASE_",
+    }
 
 
 class TelegramBotConfig(BaseSettings):
@@ -19,9 +20,10 @@ class TelegramBotConfig(BaseSettings):
     send_long_messages_as_files: bool = True
     test_mode: bool = False
 
-    class Config:
-        extra = 'ignore'
-        env_prefix = 'TELEGRAM_BOT_'
+    model_config = {
+        "extra": "ignore",
+        "env_prefix": "TELEGRAM_BOT_",
+    }
 
 
 class AppConfig(BaseSettings):
@@ -32,12 +34,12 @@ class AppConfig(BaseSettings):
     telegram_bot: TelegramBotConfig
     openai_api_key: str
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     @classmethod
     def populate_configurations(cls, values):
-        db_class = values.get('_database_config_class', DatabaseConfig)
-        tg_class = values.get('_telegram_bot_config_class', TelegramBotConfig)
+        db_class = values.get("_database_config_class", DatabaseConfig)
+        tg_class = values.get("_telegram_bot_config_class", TelegramBotConfig)
 
-        values['database'] = db_class(**values)
-        values['telegram_bot'] = tg_class(**values)
+        values["database"] = db_class(**values)
+        values["telegram_bot"] = tg_class(**values)
         return values
