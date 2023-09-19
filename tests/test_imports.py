ad36f20da80def0223c83dcf5eb05295e229b8d5
Petr Lavrov <calmquant@gmail.com>
2023-09-19 03:22:19 +0300

    make tests work add new test lib - aiogram add black

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 # todo: test mongo connection with pytest-mongodb
 
 # def test_imports():
@@ -6,7 +9,15 @@
 #     from bot_base.core.app import AppBase
 #     from bot_base.core.telegram_bot import BotBase
 
-def test_init():
+@pytest.fixture(scope='function')
+def setup_environment(monkeypatch):
+    monkeypatch.setenv('DATABASE_CONN_STR', '')
+    monkeypatch.setenv('DATABASE_NAME', 'test_db')
+    monkeypatch.setenv('TELEGRAM_BOT_TOKEN',
+                       '1234567890:aaabbbcccdd-aaabbbcccdddeee_abcdefg')
+
+
+def test_init(setup_environment):
     from bot_base.core import App, AppConfig
     app = App()
     assert isinstance(app.config, AppConfig)
